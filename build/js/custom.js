08549964669a84d4320ae728fe8c207d2567c50d
Morteza Karimi <mortezak1373@yahoo.com>
2018-08-14 01:31:44 +0430

    + Change font to IRANSansWeb, Thanks To @MoslemEbrahimi

--- build/js/custom.js
+++ build/js/custom.js
@@ -2933,29 +2933,6 @@ function init_calendar() {
 };
 
 /* DATA TABLES */
-var dataTableLang = {
-    "sEmptyTable": "هیچ داده ای در جدول وجود ندارد",
-    "sInfo": "نمایش _START_ تا _END_ از _TOTAL_ رکورد",
-    "sInfoEmpty": "نمایش 0 تا 0 از 0 رکورد",
-    "sInfoFiltered": "(فیلتر شده از _MAX_ رکورد)",
-    "sInfoPostFix": "",
-    "sInfoThousands": ",",
-    "sLengthMenu": "نمایش _MENU_ رکورد",
-    "sLoadingRecords": "در حال بارگزاری...",
-    "sProcessing": "در حال پردازش...",
-    "sSearch": "جستجو:",
-    "sZeroRecords": "رکوردی با این مشخصات پیدا نشد",
-    "oPaginate": {
-        "sFirst": "ابتدا",
-        "sLast": "انتها",
-        "sNext": "بعدی",
-        "sPrevious": "قبلی"
-    },
-    "oAria": {
-        "sSortAscending": ": فعال سازی نمایش به صورت صعودی",
-        "sSortDescending": ": فعال سازی نمایش به صورت نزولی"
-    }
-};
 
 function init_DataTables() {
 
@@ -2969,9 +2946,9 @@ function init_DataTables() {
     var handleDataTableButtons = function () {
         if ($("#datatable-buttons").length) {
             $("#datatable-buttons").DataTable({
-                language: "language": {
-                "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Persian.json"
-            },
+                "language": {
+                    "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Persian.json"
+                },
                 dom: "Bfrtip",
                 buttons: [
                     {
